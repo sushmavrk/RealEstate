2c6cbd8d26609458dbd04ee4c36bb9e7d3eac36f
Toshiba <Toshiba@DESKTOP-CSSJGEK>
2022-10-16 18:32:12 +0530

    Commit and push

---
+++ src/test/java/Gitrepo/S1.java
@@ -0,0 +1,12 @@
+package Gitrepo;
+
+public class S1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Git repository");
+		System.out.println("repository");
+		
+	}
+
+}
---
+++ src/test/java/Gitrepo/S2.java
@@ -0,0 +1,11 @@
+package Gitrepo;
+
+public class S2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("Hi World");
+	}
+
+}
